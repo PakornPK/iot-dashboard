b40dcaf24563973b76eed77c01aa970cc97d3ffe
PakornPK <sweetie_jimmie@hotmail.com>
2021-06-28 01:32:08 +0700

    update: site components and nav-draw

--- components/site/List.vue
+++ components/site/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list class="pl-14" shaped>
+  <v-list shaped>
     <v-list-item v-for="n in countSite" :key="n.id" link>
       <v-list-item-content>
         <SiteItem :name="n.name" />
@@ -8,10 +8,8 @@
     <v-list-item link @click="addSiteFunc">
       <v-list-item-content>
         <v-list-item-title>
-          <v-list-item-icon>
-            <v-icon>mdi-book-plus</v-icon>
-          </v-list-item-icon>
-          Add
+          <v-icon class="mr-5">mdi-book-plus</v-icon>
+          Add site
         </v-list-item-title>
       </v-list-item-content>
     </v-list-item>
@@ -19,28 +17,29 @@
 </template>
 
 <script>
-const makeid = (length)=> {
-    let result           = '';
-    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+const makeid = (length) => {
+  let result = ''
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
 }
 
 export default {
-  updated(){
-    localStorage.setItem('site',JSON.stringify(this.countSite))
+  updated() {
+    localStorage.setItem('site', JSON.stringify(this.countSite))
   },
-  methods:{
-    addSiteFunc(){ 
+  methods: {
+    addSiteFunc() {
       this.countSite.push({
-        id : Math.floor(Math.random()*1000000),
-        name : makeid(20)
+        id: Math.floor(Math.random() * 1000000),
+        name: makeid(20),
       })
       console.log(this.countSite)
-    }
+    },
   },
   data() {
     return {
@@ -50,5 +49,3 @@ export default {
 }
 </script>
 
-<style>
-</style>
